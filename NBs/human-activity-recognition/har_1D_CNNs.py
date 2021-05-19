71b4fb65bf474bc583821b61516e42e48881dd54
Pantelis Tzamalis <tzamalispantelis@gmail.com>
2021-05-19 15:48:54 +0300

    HAR first script version

--- NBs/human-activity-recognition/har_1D_CNNs.py
+++ NBs/human-activity-recognition/har_1D_CNNs.py
@@ -1,18 +1,13 @@
 import os
-# cnn model
+
+import tensorflow as tf
+from tensorflow import keras
 from numpy import mean
 from numpy import std
 from numpy import dstack
 from pandas import read_csv
 import pandas as pd
 from matplotlib import pyplot
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import Dropout
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
-from keras.utils import to_categorical
 
 import sys
 print("Python version")
@@ -34,8 +29,10 @@ def load_file(filepath):
 
 # load a list of files and return as a 3d numpy array
 def load_group(filenames, prefix=''):
+    print("filenames in load_group:", filenames)
     loaded = list()
     for name in filenames:
+        print("lalaaa:", prefix + name)
         data = load_file(prefix + name)
         loaded.append(data)
     # stack group so that features are the 3rd dimension
@@ -46,6 +43,7 @@ def load_group(filenames, prefix=''):
 # load a dataset group, such as train or test
 def load_dataset_group(main_app_path, group, prefix=''):
     filepath = main_app_path + "/" + "datasets/" + prefix + group + '/Inertial Signals/'
+    filepath_y_train = os.path.join(main_app_path, "datasets", prefix + group + '/y_' + group + '.txt')
     print("filepath:", filepath)
     # load all 9 files as a single array
     filenames = list()
@@ -55,27 +53,28 @@ def load_dataset_group(main_app_path, group, prefix=''):
     filenames += ['body_acc_x_' + group + '.txt', 'body_acc_y_' + group + '.txt', 'body_acc_z_' + group + '.txt']
     # body gyroscope
     filenames += ['body_gyro_x_' + group + '.txt', 'body_gyro_y_' + group + '.txt', 'body_gyro_z_' + group + '.txt']
+    print("filenames:", filenames)
     # load input data
-    X = load_group(filenames, filepath)
+    X = load_group(filenames=filenames, prefix=filepath)
     # load class output
-    y = load_file(prefix + group + '/y_' + group + '.txt')
+    y = load_file(filepath=filepath_y_train)
     return X, y
 
 
 # load the dataset, returns train and test X and y elements
 def load_dataset(main_app_path, prefix=''):
     # load all train
-    trainX, trainy = load_dataset_group(main_app_path, 'train', prefix + 'HARDataset/')
+    trainX, trainy = load_dataset_group(main_app_path, 'train', prefix + 'UCI HAR Dataset/')
     print(trainX.shape, trainy.shape)
     # load all test
-    testX, testy = load_dataset_group(main_app_path, 'test', prefix + 'HARDataset/')
+    testX, testy = load_dataset_group(main_app_path, 'test', prefix + 'UCI HAR Dataset/')
     print(testX.shape, testy.shape)
     # zero-offset class values
     trainy = trainy - 1
     testy = testy - 1
     # one hot encode y
-    trainy = to_categorical(trainy)
-    testy = to_categorical(testy)
+    trainy = tf.keras.utils.to_categorical(trainy)
+    testy = tf.keras.utils.to_categorical(testy)
     print(trainX.shape, trainy.shape, testX.shape, testy.shape)
     return trainX, trainy, testX, testy
 
@@ -84,14 +83,14 @@ def load_dataset(main_app_path, prefix=''):
 def evaluate_model(trainX, trainy, testX, testy):
     verbose, epochs, batch_size = 0, 10, 32
     n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
-    model = Sequential()
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features)))
-    model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(n_outputs, activation='softmax'))
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features)))
+    model.add(keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.MaxPooling1D(pool_size=2))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(100, activation='relu'))
+    model.add(keras.layers.Dense(n_outputs, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     # fit network
     model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
