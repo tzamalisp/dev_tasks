d6b8cdc44da03d0841ce4f8bac61fec0f56ce215
Pantelis <tzamalis@ceid.upatras.gr>
2021-05-28 15:29:08 +0300

    NB - RNNs applied to simple sequences

---
+++ utils/visualization.py
@@ -0,0 +1,33 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+mpl.rc("axes", labelsize=14)
+mpl.rc("xtick", labelsize=12)
+mpl.rc("ytick", labelsize=12)
+
+
+def plot_series(n_steps, series, y=None, y_pred=None, x_label="$t$", y_label="$x(t)$"):
+    plt.plot(series, ".-")
+    if y is not None:
+        plt.plot(n_steps, y, "bx", markersize=10)
+    if y_pred is not None:
+        plt.plot(n_steps, y_pred, "ro")
+    plt.grid(True)
+    if x_label:
+        plt.xlabel(x_label, fontsize=16)
+    if y_label:
+        plt.ylabel(y_label, fontsize=16, rotation=0)
+    plt.hlines(0, 0, 100, linewidth=1)
+    plt.axis([0, n_steps + 1, -1, 1])
+
+
+def plot_learning_curves(loss, val_loss):
+    plt.plot(np.arange(len(loss)) + 0.5, loss, "b.-", label="Training loss")
+    plt.plot(np.arange(len(val_loss)) + 1, val_loss, "r.-", label="Validation loss")
+    plt.gca().xaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
+    plt.axis([1, 20, 0, 0.05])
+    plt.legend(fontsize=14)
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.grid(True)
